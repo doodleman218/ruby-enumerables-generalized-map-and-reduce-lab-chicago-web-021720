034aeac92b091c59c170f991fb62bb21e66ba921
David Wolff <dcwolff218@gmail.com>
2020-02-12 21:20:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,7 +31,7 @@ def reduce (source_array, starting_point=nil)
    sum = starting_point
    index = 0
   else
-    starting_point = 
+    starting_point = source_array[0]
  
  new_total = 0 
   index = 0
