27d39c8522ce5939260dbd952d773c95cd905adf
David Wolff <dcwolff218@gmail.com>
2020-02-12 21:15:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,36 @@
 # Your Code Here
+# Your Code Here
+# def my_own_map (array)
+# new_array= []
+#   index = 0
+#   while index < array.length do
+#     yield
+#     index += 1
+#   end
+#   return new_array  
+# end
+
+# my_own_map (array) do
+#   |x| new_array.push(array[index] * -1 )
+# end
+
+
+def map (array)
+  new_array= []
+  index = 0
+  while index < array.length do
+    new_array.push(yield (array[index]))
+    index += 1
+  end
+   new_array  
+end
+
+def reduce (source_array, starting_point=nil)
+ new_total = 0 
+  index = 0
+  while index < source_array.length do
+    new_total += (yield(source_array[index]))
+    index += 1
+  end
+ new_total
+end
